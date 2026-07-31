30243afbbf8112ddb0c5dd35a9ee755e77f62976
echoesofwhoami <echoesofwhoami@proton.me>
2026-07-31 15:06:20 +0200

    about page layout and discord handle fix

--- src/site.config.ts
+++ src/site.config.ts
@@ -1,7 +1,7 @@
 export const siteConfig = {
   siteUrl: 'https://curlswiggerlabs.echoesofwhoami.com',
   discordInviteUrl: '',
-  discordHandle: '@echoesofwhoami',
+  discordHandle: '@echoesofwhoami_60012',
   rssPath: '/rss.xml',
   githubProfileUrl: 'https://github.com/echoesofwhoami',
   linkedinUrl: '',
